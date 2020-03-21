59a7e1ab9ca6d124ff7dd211069c364fdef50ed2
YMitrohovich <yarmitrohovich117@mail.ru>
2020-03-21 05:36:11 -0700

    Lesson 2.2: Double Overflow

--- Hello_World.c
+++ Hello_World.c
@@ -4,8 +4,8 @@ int main ()
 
 {
 	printf ("Hello World\n\n"); //Prints text Hello World on the screen
-	printf ("Press any button to exit\n"); //Also prints text on the screen
 
+	printf ("Press any button to exit\n"); //Also prints text on the screen
 	getchar (); //Requests the user to press some button to terminate the program 
 
 	return 0;
